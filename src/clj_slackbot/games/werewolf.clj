0786f0b3db2f000684ae1f6d71326cfd069deb36
lsenjov <logansenjov@gmail.com>
2016-06-29 16:16:12 +1000

    Sending of initial private messages works fine

--- src/clj_slackbot/games/werewolf.clj
+++ src/clj_slackbot/games/werewolf.clj
@@ -4,11 +4,27 @@
   (:gen-class)
   )
 
+(def ^:private all-roles
+  "A map of roles, their actions, and descriptions"
+  {:villager {:desc "You are a villager"
+              :side :town}
+   :wolf {:desc "You are a werewolf"
+          :side :wolves
+          :action {:night [:kill]}
+          }
+   :seer {:desc "You are the seer."
+          :side :town
+          :action {:night [:see]
+                   :first-night [:see]}
+          }
+   }
+  )
+
 (defn start
   "Returns an empty game map"
   []
   {:players {}
-   :status "waiting"
+   :status :waiting
    :message "Game created"}
   )
 
@@ -20,7 +36,7 @@
     (assoc gameMap :message (str "You have already joined this game " user))
     (do
       (-> gameMap 
-      (assoc-in [:players user] {:ready false})
+      (assoc-in [:players user] {:alive true})
       (assoc :message (str user " has joined the game."))
       ))))
 
@@ -40,17 +56,62 @@
   [gameMap commands {user :user channel :channel :as metaData}]
   (assoc gameMap :message
          (case (:status gameMap)
-           "waiting" (str "Waiting for players. Current players:"
-                          (apply str
-                                 (interpose ", " (keys (gameMap :players)))))
+           :waiting (str "Waiting for players. Current players: "
+                         (apply str
+                                (interpose ", " (keys (gameMap :players)))))
            "ERROR: Unknown Status")))
+
+(defn- create-roles
+  "Create a list of roles to be used in the game"
+  [n]
+  ;; Has a seer and one wolf, every 6 players adds another wolf
+  (let [part (concat '(:seer) (repeat (inc (quot n 6)) :wolf))]
+    ;; Fill the rest with villagers
+    (concat part
+            (repeat (- n (count part))
+                    :villager))))
+
+(defn- assign-single-role
+  "Assigns a single role to a player"
+  [role [pName pMap]]
+  {pName (assoc pMap :role role)}
+  )
+
+(defn- assign-roles
+  "Assigns roles to a map of players"
+  [playerMap]
+  (let [roles (create-roles (count playerMap))]
+    (apply merge (map assign-single-role roles playerMap))))
+
+(defn- single-role-description
+  "Creates a private message for a single role description"
+  [[pName pMap]]
+  {:channel pName
+   :message (get-in all-roles [(:role pMap) :desc])})
+
+(defn- trigger-roles-first-night
+  "Sends role messages out, triggers seer"
+  [gameMap]
+  ;; TODO Seer ability
+  (assoc gameMap :message (map single-role-description (:players gameMap))))
+
+(defn- begin-game
+  "Assigns roles, sets the time to first-night"
+  [gameMap]
+  (-> gameMap
+      (assoc :status :first-night)
+      (update-in [:players] assign-roles)
+      (trigger-roles-first-night)))
 
 (defn begin
   "Starts the game if there's enough players"
   [gameMap commands {user :user channel :channel :as metaData}]
-  (if (>= (count (:players gameMap)) 3)
-    ;; TODO
-    (assoc :message "Beginning game not yet implemented")
-    ;; Not enough players
-    (assoc :message (str "Not enough players. Current players: " (count (:players gameMap))))
-    ))
+  (if (= (:status gameMap) :waiting)
+    (if (>= (count (:players gameMap)) 3)
+      ;; TODO
+      (begin-game gameMap)
+      ;; Not enough players
+      (assoc :message (str "Not enough players. Current players: " (count (:players gameMap))))
+      )
+    ;; Wrong phase
+    (assoc :message "Incorrect game state to start game")))
