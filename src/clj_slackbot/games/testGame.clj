b3277891ac6d012017e34b3118856747021e2c37
lsenjov <logansenjov@gmail.com>
2016-06-29 15:26:11 +1000

    Put example joining and leaving into testGame

--- src/clj_slackbot/games/testGame.clj
+++ src/clj_slackbot/games/testGame.clj
@@ -54,4 +54,36 @@
     (do
       (-> gameMap 
       (assoc-in [:players user] {:ready false})
-      (assoc :message (str user " has joined the game."))))))
+      (assoc :message (str user " has joined the game."))
+      ))))
+
+(defn leave
+  "Leave if already in the game"
+  [gameMap commands {user :user channel :channel :as metaData}]
+  (if (get-in gameMap [:players user])
+    (-> gameMap
+        ;; Remove the user
+        (update-in [:players] dissoc user)
+        ;; Returns message
+        (assoc :message (str user " has left the game.")))
+    (assoc :message (str user " was not playing anyway."))))
+
+(defn status
+  "Show status of the current game"
+  [gameMap commands {user :user channel :channel :as metaData}]
+  (assoc gameMap :message
+         (case (:status gameMap)
+           "waiting" (str "Waiting for players. Current players:"
+                          (apply str
+                                 (interpose ", " (keys (gameMap :players)))))
+           "ERROR: Unknown Status")))
+
+(defn begin
+  "Starts the game if there's enough players"
+  [gameMap commands {user :user channel :channel :as metaData}]
+  (if (>= (count (:players gameMap)) 3)
+    ;; TODO
+    (assoc :message "Beginning game not yet implemented")
+    ;; Not enough players
+    (assoc :message (str "Not enough players. Current players: " (count (:players gameMap))))
+    ))
